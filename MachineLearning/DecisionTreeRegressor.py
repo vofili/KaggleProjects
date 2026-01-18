fbb292555e638cb75ae00b3e31ab2bf14632fe7b
valentine.ofili <support@nubeero.com>
2026-01-18 21:46:58 +0000

    separate definition of the decisiontreeregressor and pandas dataframe for melB housing data

--- MachineLearning/DecisionTreeRegressor.py
+++ MachineLearning/DecisionTreeRegressor.py
@@ -5,14 +5,19 @@ from sklearn.model_selection import train_test_split
 
 #import Data
 hse_data_file = "../datasets/melb_data.csv"
+#create dataframe and examine data
+hse_data = pd.read_csv((hse_data_file))
+print(hse_data.describe())
 
-hse_data = pd.read_csv(hse_data_file)
-#explore initial values in dataset
 print(hse_data.head())
-#explore columns(features) in the dataset
-print(hse_data.columns)
-
-# features = ['Su']
-# hse_model = DecisionTreeRegressor(random_state=1)
-#
-# hse_model.fit(X,y)
+#define features
+feature_list=['Rooms','Distance','Bedroom2','Bathroom','Car','Landsize','YearBuilt','BuildingArea']
+#define target
+y = hse_data.Price
+X = hse_data[feature_list]
+hse_price_model = DecisionTreeRegressor(random_state=1)
+hse_price_model.fit(X,y)
+print("Predict price information for these houses:")
+print(X.head())
+print("Price Information:")
+print(hse_price_model.predict(X.head()))
--- MachineLearning/PandasIntro.py
+++ MachineLearning/PandasIntro.py
@@ -13,29 +13,5 @@ y = mel_hsedata.Price
 mel_features = ['Rooms','Bathroom','Landsize','Lattitude','Longtitude']
 X = mel_hsedata[mel_features]
 #print(X)
-#print(X.describe())
-#print(X.head())
-mel_model = DecisionTreeRegressor(random_state=1)
-mel_model.fit(X,y)
-print("Making predictions for House prices:")
+print(X.describe())
 print(X.head())
-print("House Price predictions are:")
-print(mel_model.predict(X.head()))
-from sklearn.metrics import mean_absolute_error
-mae = mean_absolute_error(y,mel_model.predict(X))
-print(mae)
-
-from sklearn.model_selection import train_test_split
-train_X,val_X,train_y,val_y = train_test_split(X,y,random_state=1)
-#view validation data
-print("Valiation Data Summary")
-print(val_y.head())
-#define model
-mel_model2= DecisionTreeRegressor(random_state=1)
-#fit model to train data
-mel_model2.fit(train_X,train_y)
-#predict data
-predict_2 = mel_model2.predict(val_X)
-#get mean absolute error
-mae2 = mean_absolute_error(val_y,predict_2)
-print(mae2)
